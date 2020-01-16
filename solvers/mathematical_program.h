441a31321380cc38c80b0ee2a6d09a0fbb537c16
Yu-Ming Chen <yminchen@umich.edu>
2020-01-16 12:41:02 -0500

    Fix a bug

--- solvers/mathematical_program.h
+++ solvers/mathematical_program.h
@@ -2765,7 +2765,7 @@ class MathematicalProgram {
    *
    * See @ref variable_scaling "Variable scaling" for more information.
    */
-  void IsVariableScalingUnset(int idx_start, int idx_end);
+  bool IsVariableScalingUnset(int idx_start, int idx_end);
   //@}
 
  private:
